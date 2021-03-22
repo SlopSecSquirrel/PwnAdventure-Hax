23271f180a8471043155e42035cb02408ebf7acf
ohexfortyone <ohexfortyone>
2021-03-21 22:05:00 -0400

    Added some commands.

--- PwnAdventure Hax/PwnAdventure Hax.cpp
+++ PwnAdventure Hax/PwnAdventure Hax.cpp
@@ -5,30 +5,50 @@
 #include "framework.h"
 #include "PwnAdventure Hax.h"
 
-// Generic static helper vars 
-static std::string command; // The most recent command that the user has typed into the chat box.
-static std::set<std::string> validCommands = { "quit" }; // A list of commands that are acceptable.
-
 // Hook defs. It's GROSS that these variables need to be declared outside of the class definition, but the function pointers all break if the hooked* functions are class members.
 
 // GetTotalPlayerCount hook, hooked this because it was useful for debugging (doesn't take any arguments, returns an integer)
 const char* GetTotalPlayerCountExportName = "?GetTotalPlayerCount@GameAPI@@QAEHXZ";
 typedef int(__thiscall* RealGetTotalPlayerCount)(void*);		// MUST BE __THISCALL!!! matches the original prototype in the DLL, void* arg is 'this' keyword (because __thiscall)
 RealGetTotalPlayerCount realGetTotalPlayerCount;				// typedef'd function pointer to the actual GetTotalPlayerCount func.
-LPVOID realGetTotalPlayerCountAddr;								// The address returned by calling GetProcAddress on the GameLogic DLL.
 int  __fastcall HookedGetTotalPlayerCount(void* This, void* _);	// Prototype for our detoured method, don't ask why it needs two void pointers. __thiscall fluff
 
 // Chat hook, hooked this because we can type into the in-game chat box and give commands for the DLL to process like 'teleport x,y,z'
 const char* ChatExportName = "?Chat@Player@@UAEXPBD@Z"; 
 typedef void (__thiscall* RealChat)(void*, char const*);		// Again, first void* is 'this' variable in __thiscall calling convention
 RealChat realChat;
-LPVOID realChatAddr;
 void __fastcall HookedChat(void*, void* _, char const*);		// yes I hate the void*, void*, char const* too, __thiscall fluff again.
 
+// GetMana hook, hooked so we can return 999
+const char* GetManageExportName = "?GetMana@Player@@UAEHXZ";
+typedef int(__thiscall* RealGetMana)(void*);
+RealGetMana realGetMana;
+int __fastcall HookedGetMana(void*, void*);
+
+// GetWalkingSpeed hook, hooked so we can run VERY FAST
+const char* GetWalkingSpeedExportName = "?GetWalkingSpeed@Player@@UAEMXZ";
+typedef float(__thiscall* RealGetWalkingSpeed)(void*);
+RealGetWalkingSpeed realGetWalkingSpeed;
+float __fastcall HookedGetWalkingSpeed(void*, void*);
+
+// GetJumpSpeed hook, hooked so we can jump VERY HIGH
+const char* GetJumpSpeedExportName = "?GetJumpSpeed@Player@@UAEMXZ";
+typedef float(__thiscall* RealGetJumpSpeed)(void*);
+RealGetJumpSpeed realGetJumpSpeed;
+float __fastcall HookedGetJumpSpeed(void*, void*);
+
+// GetJumpHoldTimeExportName, hooked so we can ?????????
+const char* GetJumpHoldTimeExportName = "?GetJumpHoldTime@Player@@UAEMXZ";
+typedef float(__thiscall* RealGetJumpHoldTime)(void*);
+RealGetJumpHoldTime realGetJumpHoldTime;
+float __fastcall HookedGetJumpHoldTime(void*, void*);
+
+static CPwnAdventureHax *singleton; 
 
 // This is the constructor of a class that has been exported.
 CPwnAdventureHax::CPwnAdventureHax()
 {
+	singleton = this;
     return;
 }
 
@@ -47,6 +67,8 @@ BOOL CPwnAdventureHax::initHooks()
 	}
 	//debugHmodule(gamelogicModule);
 
+
+
 	// GetTotalPlayerCount hook setup
 	auto addrGetTotalPlayerCount = &HookedGetTotalPlayerCount;
 	auto ret = hooker(gamelogicModule, GetTotalPlayerCountExportName, (LPVOID&)addrGetTotalPlayerCount);
@@ -62,6 +84,39 @@ BOOL CPwnAdventureHax::initHooks()
 		return false;
 	}
 	realChat = (RealChat)ret;
+
+	// Player::GetMana hook setup
+	auto addrGetMana = &HookedGetMana;
+	ret = hooker(gamelogicModule, GetManageExportName, (LPVOID&)addrGetMana);
+	if (ret == nullptr) {
+		return false;
+	}
+	realGetMana = (RealGetMana)ret;
+
+	// Player::GetWalkingSpeed hook setup
+	auto addrGetWalkingSpeed = &HookedGetWalkingSpeed;
+	ret = hooker(gamelogicModule, GetWalkingSpeedExportName, (LPVOID&)addrGetWalkingSpeed);
+	if (ret == nullptr) {
+		return false;
+	}
+	realGetWalkingSpeed = (RealGetWalkingSpeed)ret;
+
+	// Player::GetJumpSpeed hook setup
+	auto addrGetJumpSpeed = &HookedGetJumpSpeed;
+	ret = hooker(gamelogicModule, GetJumpSpeedExportName, (LPVOID&)addrGetJumpSpeed);
+	if (ret == nullptr) {
+		return false;
+	}
+	realGetJumpSpeed = (RealGetJumpSpeed)ret;
+
+	// Player::GetJumpHoldTime hook setup
+	auto addrGetHoldJumpTime = &HookedGetJumpHoldTime;
+	ret = hooker(gamelogicModule, GetJumpHoldTimeExportName, (LPVOID&)addrGetHoldJumpTime);
+	if (ret == nullptr) {
+		return false;
+	}
+	realGetJumpHoldTime = (RealGetJumpHoldTime)ret;
+	 
 
 	return true;
 }
@@ -94,14 +149,14 @@ LPVOID CPwnAdventureHax::hooker(HMODULE gamelogicModule, const char* exportedFun
 void CPwnAdventureHax::startLoop()
 {
 	while (1) {
-		if ((GetAsyncKeyState(VK_RCONTROL) & 0x01) || command == "quit")
+		processCommand(command);
+		if ((GetAsyncKeyState(VK_RCONTROL) & 0x01))
 		{
 			std::wcout << L"RCONTROL pressed, attempting to exit gracefully (plz no bluescreen)" << std::endl;;
 			break;
 		}
 
-		// Clear the command var after every loop iteration.
-		command = "";
+		
 	}
 }
 
@@ -120,6 +175,39 @@ BOOL CPwnAdventureHax::cleanupHooks() {
 	return error == NO_ERROR;
 }
 
+void CPwnAdventureHax::processCommand(std::string comm)
+{
+	if (command.size() == 0) {
+		return;
+	}
+	if (command.compare("!fast on") == 0)
+	{
+		this->fastModeOn = true;
+	}
+	else if (command.compare("!fast off") == 0)
+	{
+		this->fastModeOn = false;
+	}
+	else if (command.compare("!jump on") == 0)
+	{
+		this->jumpModeOn = true;
+	}
+	else if (command.compare("!jump off") == 0)
+	{
+		this->jumpModeOn = false;
+	}
+	else if (command.compare("!hold on") == 0)
+	{
+		this->holdModeOn = true;
+	}
+	else if (command.compare("!hold off") == 0)
+	{
+		this->holdModeOn = false;
+	}
+	// Clear the command var after every loop iteration.
+	command.clear();
+}
+
 
 // HOOK CODE HOOK CODE HOOK CODE HOOK CODE \\
 // Ignore the second param, some kind of __thiscall weirdness
@@ -132,24 +220,44 @@ int  __fastcall HookedGetTotalPlayerCount(void* This, void* _)
 
 	// Get the actual result by calling the original function
 	int count = realGetTotalPlayerCount(This); 
-
-	std::cout << "Inside of HookedGetTotalPlayerCount, count is " << count << std::endl;
+	 
 	std::cout.flush();
 	return count;
 } 
 
 void __fastcall HookedChat(void* This, void* _, char const* message) {
 	
-	command = std::string(message);
+	
 	std::cout << "Inside of HookedChat, message is " << message << std::endl;
-	// Don't actually broadcast the chat message if it's a valid command. stealthy, innit
-	if (validCommands.find(command) != validCommands.end()) {
+	// Don't actually broadcast the chat message if it's a command. stealthy, innit
+	// command structure is "!fast on", "!jump off" etc.
+	if (singleton->command.at(0) == '!') {
+		singleton->command = std::string(message);
 		return;
 	}
 
 	realChat(This, message);
 }
 
+int __fastcall HookedGetMana(void* This, void* _) {	
+	int mana = realGetMana(This);
+	return 99999;
+} 
+
+float __fastcall HookedGetWalkingSpeed(void* This, void* _) {
+	int originalWalkSpeed = realGetWalkingSpeed(This);
+	return singleton->fastModeOn == true ? 60000 : originalWalkSpeed;
+}
+
+float __fastcall HookedGetJumpSpeed(void* This, void* _) {
+	int originalJumpSpeed = realGetJumpSpeed(This);
+	return singleton->jumpModeOn == true ? 10000 : originalJumpSpeed;
+}
+
+float __fastcall HookedGetJumpHoldTime(void* This, void* _) {
+	int originalJumpHoldTime = realGetJumpHoldTime(This);
+	return singleton->holdModeOn == true ? 60000 : originalJumpHoldTime;
+}
 
 // DEBUG CODE DEBUG CODE DEBUG CODE DEBUG CODE \\
 // weird hooking debug code, gratuitously robbed from https://stackoverflow.com/questions/1128150/win32-api-to-enumerate-dll-export-functions
--- PwnAdventure Hax/PwnAdventure Hax.h
+++ PwnAdventure Hax/PwnAdventure Hax.h
@@ -1,10 +1,15 @@
 // This class is exported from the dll
 class CPwnAdventureHax { 
 public:
+	BOOL fastModeOn = false;
+	BOOL jumpModeOn = false;
+	BOOL holdModeOn = false;
+	std::string command = ""; // The most recent command that the user has typed into the chat box.
 	CPwnAdventureHax(void);
 	BOOL initHooks();
 	void startLoop();
 	BOOL cleanupHooks();  
+	void processCommand(std::string);
 	LPVOID hooker(HMODULE gamelogicModule, const char* exportedFunctionName, LPVOID& hookedAddress); 
 
 	void debugHmodule(HMODULE lib);
